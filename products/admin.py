b881b03cdbee4028a94d570722768fc7335e529a
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-05-27 17:17:01 +0000

    Add product and category to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
+"Admin.py In Store App"
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    "Class to display category properties"
+    list_display = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    "Class to display product properties"
+    list_display = ['title', 'sku', 'slug', 'price', 'created_on']
+    prepopulated_fields = {'slug': ('title',)}
+    list_editable = ['price', ]
